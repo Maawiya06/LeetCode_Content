f7a8d9dd2137f56a7390f78b31c025a595ef9577
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-05-25 01:32:00 +0530

    Time: 148 ms (5.03%) | Memory: 58.1 MB (18.67%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    
+    public int[] nextSmallerElement(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = arr[i];
+            while (st.peek() != -1 && arr[st.peek()] >= curr) {
+                st.pop();
+            }
+            ans[i] = st.peek();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public int[] prevSmallerElement(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+
+        for (int i = 0; i < n; i++) {
+            int curr = arr[i];
+            while (st.peek() != -1 && arr[st.peek()] >= curr) {
+                st.pop();
+            }
+            ans[i] = st.peek();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+
+        int[] next = nextSmallerElement(heights);
+        int[] prev = prevSmallerElement(heights);
+
+        // Update -1 in 'next' with length (i.e., right end of histogram)
+        for (int i = 0; i < n; i++) {
+            if (next[i] == -1) {
+                next[i] = n;
+            }
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = next[i] - prev[i] - 1;
+            int length = heights[i];
+            int currArea = width * length;
+            maxArea = Math.max(maxArea, currArea);
+        }
+
+        return maxArea;
+    }
+}
