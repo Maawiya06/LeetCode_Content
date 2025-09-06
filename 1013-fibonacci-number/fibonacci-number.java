eafb14d2ea15530e16d9a2d4f436cfd9a7af2559
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-09-06 17:30:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (81.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,35 @@
 class Solution {
+    public int solveusingMem(int n, ArrayList<Integer> dp){
+        //base case
+        if(n == 0 || n == 1){
+            return n;
+        }
+        
+        // ak br solve kr lya dobara solve nhi krna h 
+        if(dp.get(n) != -1){
+            return dp.get(n);
+        }
+
+        dp.set(n, solveusingMem(n - 1, dp) + solveusingMem(n - 2, dp));
+        return dp.get(n);
+    }
     public int fib(int n) {
         
         //base case
         // procesing
         // recursive relation
-        if(n == 0 || n == 1){
-            return n;
-        }
+        // if(n == 0 || n == 1){
+        //     return n;
+        // }
 
-        int ans = fib(n - 1) + fib(n - 2);
+        // int ans = fib(n - 1) + fib(n - 2);
+        // return ans;
+
+        ArrayList<Integer> dp = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            dp.add(-1);
+        }
+        int ans = solveusingMem(n, dp);
         return ans;
     }
 }
