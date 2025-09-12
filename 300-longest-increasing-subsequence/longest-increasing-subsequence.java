88c0479477b119429ab9351a0a883f11bfebd668
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-09-12 10:14:24 +0530

    Time: 7 ms (80.90%) | Memory: 44.8 MB (33.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -17,15 +17,37 @@ class Solution {
         dp[curr][prev + 1] = Math.max(include, exclude);
         return dp[curr][prev + 1];
     }
+
+
+    int solveByBinarySearch(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+        ans.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > ans.get(ans.size() - 1)) {
+                ans.add(nums[i]);
+            } else {
+                // find just-bigger element index (lower_bound equivalent)
+                int index = Collections.binarySearch(ans, nums[i]);
+                if (index < 0) {
+                    index = -(index + 1);
+                }
+                ans.set(index, nums[i]);
+            }
+        }
+        return ans.size();
+    }
+
     public int lengthOfLIS(int[] nums) {
         int curr = 0;
         int prev = -1;
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        int ans = solve(nums, curr, prev, dp);
+        // int[][] dp = new int[n + 1][n + 1];
+        // for(int i = 0; i < n; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        int ans = solveByBinarySearch(nums);
         return ans;
     }
 }
