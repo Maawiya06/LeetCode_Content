dd179bb8dd7c991148e305c3d526917cda7f1389
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-09-13 19:55:30 +0530

    Time: 15 ms (70.05%) | Memory: 43.4 MB (6.87%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.java
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.java
@@ -16,6 +16,32 @@ class Solution {
         dp[start][end] = ans;
         return dp[start][end];
     }
+
+    int solveUsingTabulation(int n){
+        //create dp
+        int[][] dp = new int[n + 2][n + 1];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], 0);
+        }
+        
+        for(int start_index = n; start_index >= 1; start_index--){
+            for(int end_index = 1; end_index <= n; end_index++){
+
+                // base case
+                if(start_index >= end_index){
+                    continue;
+                }
+                int ans = Integer.MAX_VALUE;
+                for(int i = start_index; i <= end_index; i++){
+                    ans = Math.min(ans, i + Math.max(dp[start_index][i - 1], dp[i + 1][end_index]));
+                }
+
+                dp[start_index][end_index] = ans;
+            }
+        }
+        return dp[1][n];
+
+    }
     public int getMoneyAmount(int n) {
         int start = 1;
         int end = n;
@@ -24,7 +50,7 @@ class Solution {
         for(int i = 0; i < n; i++){
             Arrays.fill(dp[i], -1);
         }
-        int ans = solve(start, end, dp);
+        int ans = solveUsingTabulation(n);
         return ans;
     }
 }
