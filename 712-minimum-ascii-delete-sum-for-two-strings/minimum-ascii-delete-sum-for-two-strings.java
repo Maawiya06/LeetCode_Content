dc708464cbeb30193d5d93e2f7183e8aaf16c8d4
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-02 14:34:44 +0530

    Time: 21 ms (55.78%) | Memory: 45 MB (49.84%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -25,13 +25,48 @@ class Solution {
         dp[i][j] = cost;
         return dp[i][j];
     }
+
+
+    int solveByTabulation(String s1, String s2){
+       int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+       for(int i = 0; i <= s1.length(); i++){
+            Arrays.fill(dp[i], 0);
+        }
+        // base case
+        for(int i = s1.length() - 1; i >= 0; i--){
+            dp[i][s2.length()] = s1.charAt(i) + dp[i + 1][s2.length()];
+        }
+        for(int i = s2.length() - 1; i >= 0; i--){
+            dp[s1.length()][i] = s2.charAt(i) + dp[s1.length()][i + 1];
+        }
+
+
+        for(int i = s1.length() - 1; i >= 0; i--){
+            for(int j = s2.length() - 1; j >= 0; j--){
+                int cost = 0;
+                if(s1.charAt(i) == s2.charAt(j)){
+                    cost = dp[i + 1][j + 1];
+                }
+                else{
+                    int cost1 = s1.charAt(i) + dp[i + 1][j];
+                    int cost2 = s2.charAt(j) + dp[i][j + 1];
+                    cost = Math.min(cost1, cost2);
+                }
+                dp[i][j] = cost;
+            }
+        }
+        return dp[0][0];
+    }
     public int minimumDeleteSum(String s1, String s2) {
 
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        // int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
-        for(int i = 0; i <= s1.length(); i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return solve(s1, s2, 0, 0, dp);
+        // for(int i = 0; i <= s1.length(); i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return solve(s1, s2, 0, 0, dp);
+
+        return solveByTabulation(s1, s2);
     }
 }
