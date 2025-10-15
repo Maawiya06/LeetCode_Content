dda0e25f5519bd9f24e8d4daf0ddbd9376a6224a
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-15 22:45:57 +0530

    Time: 5 ms (29.65%) | Memory: 41.9 MB (69.63%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,35 +1,49 @@
 class Solution {
-    List<String> solve(String s, Map<String, Boolean> dict, int i) {
+    List<String> solve(String s, Map<String, Boolean> dict, int i, Map<Integer, List<String>> dp) {
         if (i == s.length()) {
             List<String> base = new ArrayList<>();
-            base.add(""); 
+            base.add("");
             return base;
         }
 
+        if (dp.containsKey(i)) {
+            return dp.get(i);
+        }
+
         List<String> ans = new ArrayList<>();
-        String word = "";
-
+        StringBuilder wordBuilder = new StringBuilder();
+        
         for (int j = i; j < s.length(); j++) {
-            word += s.charAt(j);
+            wordBuilder.append(s.charAt(j));
+            String word = wordBuilder.toString();
 
-            if (!dict.containsKey(word)) continue;
+            if (!dict.containsKey(word)) {
+                continue;
+            }
 
-            List<String> right = solve(s, dict, j + 1);
+            List<String> right = solve(s, dict, j + 1, dp);
 
+            
             for (String eachRightPart : right) {
-                if (eachRightPart.equals(""))
-                    ans.add(word);
-                else
+                if (eachRightPart.equals("")) {
+                    ans.add(word); 
+                } else {
                     ans.add(word + " " + eachRightPart);
+                }
             }
         }
+        
+        
+        dp.put(i, ans);
         return ans;
     }
 
     public List<String> wordBreak(String s, List<String> wordDict) {
         Map<String, Boolean> dict = new HashMap<>();
-        for (String word : wordDict) dict.put(word, true);
-
-        return solve(s, dict, 0);
+        for (String word : wordDict) {
+            dict.put(word, true);
+        }
+        Map<Integer, List<String>> dp = new HashMap<>();
+        return solve(s, dict, 0, dp);
     }
 }
