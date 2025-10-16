312f0f384317e9b2a47f4b06e7498e06ba326a05
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-16 19:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (17.58%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,18 +1,23 @@
 class Solution {
-    int solve(int start, int end){
+    int solve(int start, int end, int[][] dp){
         if(start > end) return 1;
         if(start == end) return 1;
         
+        if(dp[start][end] != -1) return dp[start][end];
         int ans = 0;
         for(int i = start; i <= end; i++){
-            int left = solve(start, i - 1);
-            int right = solve(i + 1, end);
+            int left = solve(start, i - 1, dp);
+            int right = solve(i + 1, end, dp);
 
             ans = ans + left * right;
         }
-        return ans;
+        return dp[start][end] = ans;
     }
     public int numTrees(int n) {
-        return solve(1, n);
+        int[][] dp = new int[n][n + 1];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(1, n, dp);
     }
 }
