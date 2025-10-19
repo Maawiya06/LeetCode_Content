b6dd83921b95c7d1392c10180b1e14de03ee327c
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-19 20:52:51 +0530

    Time: 17 ms (24.47%) | Memory: 45 MB (13.65%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    int solve(int[] piles, int i, int M, boolean alice, int[][][] dp) {
+        if (i == piles.length) return 0;
+
+        int index = alice ? 1 : 0;
+
+        if (dp[i][M][index] != -1) {
+            return dp[i][M][index];
+        }
+
+        int ans = alice ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int total = 0;
+
+        for (int X = 1; X <= 2 * M; ++X) {
+            if (i + X > piles.length) break; 
+            total += piles[i + X - 1];
+            if (alice) {
+                ans = Math.max(ans, total + solve(piles, i + X, Math.max(M, X), false, dp));
+            } else {
+                ans = Math.min(ans, solve(piles, i + X, Math.max(M, X), true, dp));
+            }
+        }
+
+        dp[i][M][index] = ans;
+        return ans;
+    }
+
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        int[][][] dp = new int[n + 1][n + 1][2];
+
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        return solve(piles, 0, 1, true, dp);
+    }
+}
