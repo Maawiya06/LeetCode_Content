75b2cb0dedfe7fb682efdca371e378ba6b5d0172
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-19 21:09:18 +0530

    Time: 46 ms (7.29%) | Memory: 45.4 MB (7.53%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -24,18 +24,55 @@ class Solution {
         dp[i][M][index] = ans;
         return ans;
     }
-
-    public int stoneGameII(int[] piles) {
+    
+    // solve by Bottom Up
+    int solveBU(int[] piles){
         int n = piles.length;
-        int[][][] dp = new int[n + 1][n + 1][2];
 
-        
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int[][][] dp = new int[n + 1][n + 1][2];
+        for(int i = 0; i <= n; i++){
+            for(int j = i; j <= n; j++){
+                Arrays.fill(dp[i][j], 0);
             }
         }
 
-        return solve(piles, 0, 1, true, dp);
+        for(int i = n - 1; i >= 0; i--){
+            for(int M = n; M >= 1; M--){
+                for(int aliceint = 0; aliceint <= 1; aliceint++){
+                    
+                    // int to boolean 
+                    boolean alice = (aliceint == 1);
+
+                    int ans = alice ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                    int total = 0;
+                    for(int X = 1; X <= 2 * M; X++){
+                        if (i + X > piles.length) break; 
+                        total += piles[i + X - 1];
+                        if (alice) {
+                            ans = Math.max(ans, total + dp[i + X][Math.max(X, M)][0]);
+                        } else {
+                            ans = Math.min(ans, dp[i + X][Math.max(X, M)][1]);
+                        }
+                    }
+                    dp[i][M][aliceint] = ans;
+                }
+            }
+        }
+        return dp[0][1][1];
+    }
+    public int stoneGameII(int[] piles) {
+        // int n = piles.length;
+        // int[][][] dp = new int[n + 1][n + 1][2];
+
+        
+        // for (int i = 0; i <= n; i++) {
+        //     for (int j = 0; j <= n; j++) {
+        //         Arrays.fill(dp[i][j], -1);
+        //     }
+        // }
+
+        // return solve(piles, 0, 1, true, dp);
+
+        return solveBU(piles);
     }
 }
