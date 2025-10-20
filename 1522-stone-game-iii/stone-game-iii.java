0d2f08c8cda05d53518ac41d280cb2f52f20634e
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-20 10:32:15 +0530

    Time: 8 ms (87.99%) | Memory: 55.2 MB (98.69%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -14,13 +14,36 @@ class Solution {
         }
         return dp[i] = ans;
     }
+
+    // Bottom Up dp
+    int solveBU(int[] sv){
+        int n = sv.length;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 0);
+
+        for(int i = n - 1; i>=0; i--){
+            int ans = Integer.MIN_VALUE;
+            int total = 0;
+
+            for(int X = 1; X <= 3; X++){
+                if(i + X - 1 >= n) break;
+                total = total + sv[i + X - 1];
+                ans = Math.max(ans, total - dp[i + X]);
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
+    }
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
 
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
 
-        int ans = solve(stoneValue, 0, dp);
+        // int ans = solve(stoneValue, 0, dp);
+
+        int ans = solveBU(stoneValue);
 
         if(ans > 0) return "Alice";
         if(ans < 0) return "Bob";
