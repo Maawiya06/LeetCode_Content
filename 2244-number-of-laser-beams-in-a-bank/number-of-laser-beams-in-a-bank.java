148b2c82b8fd77b6db5fcd10c6a40b5257659a9b
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-10-27 23:23:58 +0530

    Time: 9 ms (94.80%) | Memory: 45.3 MB (45.58%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,19 +1,41 @@
 class Solution {
-    public int numberOfBeams(String[] bank) {
-        int totalBeam = 0;
-        int PDeviceCount = 0;
-
-        for(String r : bank){
-            int CDeviceCount = 0;
-            for(int i = 0; i < r.length(); i++){
-                if(r.charAt(i) == '1') CDeviceCount++;
-            }
-
-            if(CDeviceCount > 0){
-                totalBeam = totalBeam + PDeviceCount * CDeviceCount;
-                PDeviceCount = CDeviceCount;
-            }
+    public int CountDevice(String row){
+        int count = 0;
+        for(int i = 0; i < row.length(); i++){
+            if(row.charAt(i) == '1') count++;
         }
-        return totalBeam;
+        return count;
+    }
+
+    private int solve(String[] bank, int rowIndex, int PDeviceCount){
+        if(rowIndex == bank.length) return 0;
+
+        int CDeviceCount = CountDevice(bank[rowIndex]);
+        if(CDeviceCount > 0){
+            int beam = PDeviceCount * CDeviceCount;
+            return beam + solve(bank, rowIndex + 1, CDeviceCount);
+        }
+        else{
+            return solve(bank, rowIndex + 1, PDeviceCount);
+        }
+    }
+    public int numberOfBeams(String[] bank) {
+        // int totalBeam = 0;
+        // int PDeviceCount = 0;
+
+        // for(String r : bank){
+        //     int CDeviceCount = 0;
+        //     for(int i = 0; i < r.length(); i++){
+        //         if(r.charAt(i) == '1') CDeviceCount++;
+        //     }
+
+        //     if(CDeviceCount > 0){
+        //         totalBeam = totalBeam + PDeviceCount * CDeviceCount;
+        //         PDeviceCount = CDeviceCount;
+        //     }
+        // }
+        // return totalBeam;
+
+        return solve(bank, 0, 0);
     }
 }
