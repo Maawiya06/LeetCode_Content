fe492706528fdd484a76539a01e120a09260deed
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-01-11 22:08:54 +0530

    Time: 20 ms (14.02%) | Memory: 45.5 MB (50.36%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,16 +1,39 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         
+        // Arrays.sort(nums);
+        // int ans = nums[0] + nums[1] + nums[2];
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         for(int k = j + 1; k < nums.length; k++){
+        //             int sum = nums[i] + nums[j] + nums[k];
+        //             if(Math.abs(sum - target) < Math.abs(ans - target)){
+        //                 ans = sum;
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+
         Arrays.sort(nums);
-        int ans = nums[0] + nums[1] + nums[2];
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                for(int k = j + 1; k < nums.length; k++){
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if(Math.abs(sum - target) < Math.abs(ans - target)){
-                        ans = sum;
-                    }
+        int n = nums.length;
+        int ans = 0;
+        int maxDiff = Integer.MAX_VALUE;
+        for(int i = 0; i < n - 2; i++){
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = Math.abs(target - sum);
+                if(diff < maxDiff){
+                    maxDiff = diff;
+                    ans = sum;
                 }
+                if(sum == target){
+                    return sum;
+                }
+                else if(sum < target) left++;
+                else right--;
             }
         }
         return ans;
