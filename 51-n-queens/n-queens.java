b07ee1f6b09e97735695d1630460f3b5c587dc03
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-01-24 21:30:06 +0530

    Time: 2 ms (79.84%) | Memory: 46.6 MB (73.84%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,47 @@
+class Solution {
+    private boolean isPresent(int n, char[][] QueenPresent, int row, int col){
+
+        for(int i = 0; i < n; i++){
+            if(QueenPresent[i][col] == 'Q') return false;
+        }
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+            if(QueenPresent[i][j] == 'Q') return false;
+        }
+        for(int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++){
+            if(QueenPresent[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    private void solveForQueens(int n, List<List<String>> ans, char[][] QueenPresent, int row){
+        if(row == n){
+            List<String> result = new ArrayList<>();
+            for(char[] rowArray : QueenPresent){
+                result.add(new String(rowArray));
+            }
+            ans.add(result);
+            return;
+        }
+
+        for(int col = 0; col < n; col++){
+            if(isPresent(n, QueenPresent, row, col)){
+                // add queen
+                QueenPresent[row][col] = 'Q';
+                // recursively solve
+                solveForQueens(n, ans, QueenPresent, row + 1);
+                // backtrack: remove queen
+                QueenPresent[row][col] = '.';
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        char[][] QueenPresent = new char[n][n];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(QueenPresent[i], '.');
+        }
+
+        solveForQueens(n, ans, QueenPresent, 0);
+        return ans;
+    }
+}
