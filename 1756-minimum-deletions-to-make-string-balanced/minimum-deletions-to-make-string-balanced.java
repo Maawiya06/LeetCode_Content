efd2d4a43dfdb483723192c544c490ce1f0fdb7a
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-02-07 19:07:11 +0530

    Time: 218 ms (5.26%) | Memory: 166.7 MB (5.59%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int solve(String s, int index, int Bcollect, Integer[][] dp){
+        if(index == s.length()) return 0;
+
+        if(dp[index][Bcollect] != null) return dp[index][Bcollect];
+
+        char ch = s.charAt(index);
+
+        int delete = 1 + solve(s, index + 1, Bcollect, dp);
+        int ans;
+
+        if(ch == 'a'){
+            if(Bcollect == 1){
+                ans = Integer.MAX_VALUE;
+            }
+            else{
+                ans = solve(s, index + 1, 0, dp);
+            }
+        }
+        else{
+            ans = solve(s, index + 1, 1, dp);
+        }
+        return dp[index][Bcollect] = Math.min(delete, ans);
+    }
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        Integer[][] dp = new Integer[n][2];
+        return solve(s, 0, 0, dp);
+    }
+}
