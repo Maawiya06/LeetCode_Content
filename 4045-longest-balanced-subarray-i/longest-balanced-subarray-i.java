85742410ab7e51011a10bf36553b950350e29ac3
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-02-10 12:48:10 +0530

    Time: 299 ms (77.55%) | Memory: 47 MB (68.61%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,33 +1,51 @@
 class Solution {
     public int longestBalanced(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
+        // int maxlen = 0;
+
+        // for(int i = 0; i < n; i++){
+        //     boolean[] even = new boolean[100001];
+        //     boolean[] odd = new boolean[100001];
+
+        //     int evencount = 0, oddcount = 0;
+
+        //     for(int j = i; j < nums.length; j++){
+        //         int ans = nums[j];
+
+        //         if(ans % 2 == 0){
+        //             if(!even[ans]){
+        //                 even[ans] = true;
+        //                 evencount++;
+        //             }
+        //         }
+        //         else{
+        //             if(!odd[ans]){
+        //                 odd[ans] = true;
+        //                 oddcount++;
+        //             }
+        //         }
+
+        //         if(evencount == oddcount){
+        //             int totalLength = j - i + 1;
+        //             if(totalLength > maxlen) maxlen = totalLength;
+        //         }
+        //     }
+        // }
+        // return maxlen;
+
+        // 2nd way hashmap
+
         int maxlen = 0;
-
-        for(int i = 0; i < n; i++){
-            boolean[] even = new boolean[100001];
-            boolean[] odd = new boolean[100001];
-
-            int evencount = 0, oddcount = 0;
+        for(int i = 0; i < nums.length; i++){
+            HashSet<Integer> evencount = new HashSet<>();
+            HashSet<Integer> oddcount = new HashSet<>();
 
             for(int j = i; j < nums.length; j++){
-                int ans = nums[j];
+                if(nums[j] % 2 == 0) evencount.add(nums[j]);
+                else oddcount.add(nums[j]);
 
-                if(ans % 2 == 0){
-                    if(!even[ans]){
-                        even[ans] = true;
-                        evencount++;
-                    }
-                }
-                else{
-                    if(!odd[ans]){
-                        odd[ans] = true;
-                        oddcount++;
-                    }
-                }
-
-                if(evencount == oddcount){
-                    int totalLength = j - i + 1;
-                    if(totalLength > maxlen) maxlen = totalLength;
+                if(evencount.size() == oddcount.size()){
+                    maxlen = Math.max(maxlen, j - i + 1);
                 }
             }
         }
