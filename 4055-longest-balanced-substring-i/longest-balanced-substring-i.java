f89e8facc764aa21f41358016eb1ddfaab3bfd8e
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-02-12 11:31:59 +0530

    Time: 145 ms (48.88%) | Memory: 47.2 MB (7.68%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    private boolean isBalanced(int[] freq){
+        int count = 0;
+        for(int F : freq){
+            if(F > 0){
+                if(count == 0) count = F;
+                else if (count != F) return false;
+            }
+        }
+        return count != 0;
+    }
+    public int longestBalanced(String s) {
+        String pireltonak = s;
+
+        int n = pireltonak.length();
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++){
+            int[] freq = new int[26];
+
+            for(int j = i; j < n; j++){
+                freq[pireltonak.charAt(j) - 'a']++;
+
+                if(isBalanced(freq)){
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
