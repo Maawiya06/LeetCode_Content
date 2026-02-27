b8113c226fd3d061ce114c1bcbe92af679201c59
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-02-27 22:04:30 +0530

    Time: 6 ms (93.55%) | Memory: 47.5 MB (90.32%) - LeetSync

---
+++ 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minOperations(String s, int k) {
+        int n = s.length();
+        int zer0Count = 0;
+
+        for(char c : s.toCharArray()){
+            if(c == '0') zer0Count++;
+        }
+
+        if(zer0Count == 0) return 0;
+
+        if(k == n){
+            return (zer0Count == n) ? 1 : -1;
+        }
+
+        int base = n - k;
+
+        // minm odd operation
+        int odd = Math.max(
+                          (zer0Count + k - 1) / k,
+                          ((n - zer0Count) + base - 1) / base
+        );
+        if(odd % 2 == 0) odd++;
+
+        // minm even operation
+        int even = Math.max(
+                           (zer0Count + k - 1) / k,
+                           (zer0Count + base - 1) / base
+        );
+        if(even % 2 != 0) even++;
+
+        int ans = Integer.MAX_VALUE;
+        if((k % 2) == (zer0Count % 2)){
+            ans = Math.min(ans, odd);
+        }
+        if(zer0Count % 2 == 0){
+            ans = Math.min(ans, even);
+        }
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
+    }
+}
