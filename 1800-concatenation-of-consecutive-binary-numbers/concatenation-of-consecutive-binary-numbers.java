1d1a1a97fd4a3d52f65ac25b95ec6ca8388322f3
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-02-28 22:37:38 +0530

    Time: 41 ms (50.00%) | Memory: 42.3 MB (84.88%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -1,14 +1,26 @@
 class Solution {
     public int concatenatedBinary(int n) {
-        String str = "";
-        int mod = 1000000007;
-        for(int i = 0; i <= n; i++){
-            String binary = Integer.toBinaryString(i);
-            str = str + binary;
-            long result = Long.parseLong(str, 2);
-            result = result % mod;
-            str = Long.toBinaryString(result);
+        // optimised 
+        long ans = 0;
+        int bit = 0;
+        int mod = 1_000_000_007;
+
+        for(int i = 1; i <= n; i++){
+            if((i & (i - 1)) == 0){
+                bit++;
+            }
+            ans = ((ans << bit) % mod + i) % mod;
         }
-        return (int)Long.parseLong(str, 2);
+        return (int)ans;
+        // String str = "";
+        // int mod = 1000000007;
+        // for(int i = 0; i <= n; i++){
+        //     String binary = Integer.toBinaryString(i);
+        //     str = str + binary;
+        //     long result = Long.parseLong(str, 2);
+        //     result = result % mod;
+        //     str = Long.toBinaryString(result);
+        // }
+        // return (int)Long.parseLong(str, 2);
     }
 }
