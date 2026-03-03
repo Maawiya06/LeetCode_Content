2ff6f179063930a693f161c20a84b1693c4d9e65
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-03-03 22:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (57.48%) - LeetSync

---
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.java
@@ -0,0 +1,39 @@
+class Solution {
+    // 1> greddy apprach
+    // private int solve(int target, int maxDoubles){
+    //     int steps = 0;
+
+    //     while(target > 1 && maxDoubles > 0){
+    //         if(target % 2 == 0){
+    //             target = target / 2;
+    //             maxDoubles--;
+    //         }
+    //         else{
+    //             target = target - 1;
+    //         }
+    //         steps++;
+    //     }
+    //     if(target > 1){
+    //         steps = steps + (target - 1);
+    //     }
+    //     return steps;
+    // }
+
+    // 2> backward resursion
+    private int solve(int target, int maxDoubles){
+        //base case
+        if(target == 1) return 0;
+
+        // if no double left 
+        if(maxDoubles == 0) return target - 1;
+
+        // if even
+        if(target % 2 == 0) return 1 + solve(target / 2, maxDoubles - 1);
+
+        // if odd
+        return 1 + solve(target - 1, maxDoubles);
+    }
+    public int minMoves(int target, int maxDoubles) {
+        return solve(target, maxDoubles);
+    }
+}
