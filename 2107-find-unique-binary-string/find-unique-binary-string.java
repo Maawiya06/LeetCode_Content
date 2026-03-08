35216704e72b18756ffdcd3b560b6738d25febf1
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-03-08 21:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (37.32%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,20 +1,25 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
+        // int size = (int)Math.pow(2, n);
+        // int[] ans = new int[size];
+        // for(String num: nums){
+        //     int val = Integer.parseInt(num, 2);
+        //     ans[val]++;
+        // }
 
-        int size = (int)Math.pow(2, n);
-        int[] ans = new int[size];
-        for(String num: nums){
-            int val = Integer.parseInt(num, 2);
-            ans[val]++;
-        }
+        // for(int i = 0; i < size; i++){
+        //     if(ans[i] == 0){
+        //         String result = Integer.toBinaryString(i);
+        //         return "0".repeat(n - result.length()) + result;
+        //     }
+        // }
+        // return "0".repeat(n);
 
-        for(int i = 0; i < size; i++){
-            if(ans[i] == 0){
-                String result = Integer.toBinaryString(i);
-                return "0".repeat(n - result.length()) + result;
-            }
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < nums.length; i++){
+            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
-        return "0".repeat(n);
+        return ans.toString();
     }
 }
