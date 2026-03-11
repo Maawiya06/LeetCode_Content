5a70fa2a8d4dacd46e180b9c440d4ddcee321838
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-03-11 21:24:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (98.98%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+        int ans = (1 << (32 - Integer.numberOfLeadingZeros(n))) - 1;
+        return n ^ ans;
+    }
+}
